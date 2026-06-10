9791846e005ad5da34ca810c35c27be1b89d2092
Emmanuel <emmcast91@gmail.com>
2026-06-09 21:13:01 -0600

    feat: se implementan las vistas al seleccionar

--- src/app/modules/recommendations/pages/recommendation-detail/recommendation-detail.component.ts
+++ src/app/modules/recommendations/pages/recommendation-detail/recommendation-detail.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RecommendationModel } from '../../models/recommendation.model';
+import { RecommendationService } from '../../services/recommendation.service';
 
 @Component({
   selector: 'app-recommendation-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './recommendation-detail.component.html',
-  styleUrl: './recommendation-detail.component.css'
+  styleUrls: ['./recommendation-detail.component.css']
 })
-export class RecommendationDetailComponent {
+export class RecommendationDetailComponent implements OnInit {
 
+  recommendation?: RecommendationModel;
+  type: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private recommendationService: RecommendationService
+  ) {}
+
+  ngOnInit(): void {
+    this.type = this.route.snapshot.paramMap.get('type') || '';
+    const title = this.route.snapshot.paramMap.get('title') || '';
+
+    this.recommendation = this.recommendationService.getByTitle(title);
+  }
+
+  goBack(): void {
+    this.router.navigate(['/recommendations', this.type]);
+  }
+
+  logout(): void {
+    localStorage.removeItem('currentUser');
+    this.router.navigate(['/login']);
+  }
 }
